a38513f862f8502e79c04e6bff68a41b72bd173e
Shaheer <shaheer@pop-os.localdomain>
2024-05-10 18:33:17 +0500

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import Welcome from './pages/Welcome/Welcome';
+import SignIn from './pages/SignIn/SignIn';
+import TnC from './pages/TnC/TnC';
+import SignUp from './pages/SignUp/SignUp';
+import './App.css';
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Smug + Smug = 2 Smug</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router> {/* Wrap your routes with BrowserRouter */}
+      <Routes>
+        <Route exact path="/SignUp" element={<SignUp />} />
+        <Route path="/" element={<SignIn />} />
+        <Route path="/TnC" element={<TnC />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
+/*
+const express = require("express")
+const collection = require("./mongo")
+const cors = require("cors")
+const app = express()
+
+app.use(express.json)
+app.use(express.urlencoded({extended : true}))
+app.use("cors")
+
+app.get("/",cors(),(req,res)=>{
+
+})
+
+app.post("/",async(req,res)=>{
+  const{username,email,password}=req.body
+
+  try{
+    const check=await collection.findOne({email:email})
+
+    if (check){
+      res.json("exist")
+    }
+    else{
+      res.json("not exist")
+    }
+
+  }
+  catch(e){
+    console.log(e)
+    res.json("not exist")
+
+  }
+})
+
+
+app.post("/signup",async(req,res)=>{
+  const{username,email,password}=req.body
+
+  const data={
+    username:username
+    email:email,
+    password:password
+  }
+
+  try{
+    const check=await collection.findOne({email:email})
+
+    if (check){
+      res.json("exist")
+    }
+    else{
+      res.json("not exist")
+      await collection.insertMany([data])
+    }
+
+  }
+  catch(e){
+    console.log(e)
+    res.json("not exist")
+
+  }
+})
+
+app.listen(8000,()=>{
+  console.log("port connected")
+})
+*/
